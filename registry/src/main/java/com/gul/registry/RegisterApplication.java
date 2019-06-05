b61d16fe07505b1872023a84981416d84b91f275
gul <1064970287@qq.com>
2019-06-06 00:30:33 +0800

    修改

--- registry/src/main/java/com/gul/registry/RegisterApplication.java
+++ registry/src/main/java/com/gul/registry/RegisterApplication.java
@@ -3,9 +3,11 @@ package com.gul.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrixDashboard
 public class RegisterApplication {
 
 	public static void main(String[] args) {
--- user/src/main/java/com/gul/user/web/UserRestController.java
+++ user/src/main/java/com/gul/user/web/UserRestController.java
@@ -1,8 +1,9 @@
 package com.gul.user.web;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -24,6 +25,14 @@ public class UserRestController {
         Map<String,String> map=new HashMap();
         map.put("userName","Gul");
         map.put("email","111111@qq.com");
+        return map;
+    }
+
+    @GetMapping(value = "cmd",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @HystrixCommand
+    public Map<String,String> getUser(){
+        Map<String,String> map=new HashMap();
+        map.put("userName","Gul");
         return map;
     }
 }
